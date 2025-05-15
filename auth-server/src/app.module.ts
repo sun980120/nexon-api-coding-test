d7ef076af849af13a209bf3bf7a12eadd7ca06a2
WooSungHong <ws.hong@intin.kr>
2025-05-15 17:20:17 +0900

    chore: initial auth-service project setup

---
+++ auth-server/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            expandVariables: true,
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                uri: configService.get<string>('MONGO_URI'),
+            }),
+        })
+    ],
+})
+export class AppModule {}
